a8de28485fb8d8c49f5140e36fdee97a458588e5
Pranesh <praneshmadhan646@gmail.com>
2025-09-02 12:00:55 +0530

    added type check and commented it in main.go file
    Co-authored-by: Pranesh S <praneshpalamedu17@gmail.com>

--- booking-app/main.go
+++ booking-app/main.go
@@ -7,6 +7,7 @@ func main(){
 	var confName ="Learning Go"
 	const noOfTickets = 64
 	var remTickets = 64
+	// fmt.Printf("Type of confName is %T,noOfTickets is %T,remTickets is %T\n",confName,noOfTickets,remTickets)
 	fmt.Printf("Welcome to %v booking app\n", confName)
 	fmt.Printf("We have %v tickets and %v tickets remaining, Hurry UP!!\n",noOfTickets,remTickets)
 	fmt.Println("Get Your tickets")
