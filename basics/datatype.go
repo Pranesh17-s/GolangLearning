611617058523b489552b4aab6c69cfc13ee83e05
Pranesh <praneshmadhan646@gmail.com>
2025-09-04 11:10:13 +0530

    added about integer datatype basic arthimetic operation in go with comments for understanding
    Co-authored-by: Pranesh S <praneshpalamedu17@gmail.com>

--- basics/datatype.go
+++ basics/datatype.go
@@ -27,6 +27,19 @@ func main() {
 	fmt.Println("int32:", int32Var)
 	fmt.Println("int64:", int64Var)
 	fmt.Println()
-
-	
+	// Basic Arithmetic Operations
+	var x int16 = 170
+    var y int16 = 83
+    //Addition
+    fmt.Printf(" addition :  %d + %d = %d\n ", x, y, x+y)
+    //Subtraction
+    fmt.Printf("subtraction : %d - %d = %d\n", x, y, x-y)
+    //Multiplication
+    fmt.Printf(" multiplication : %d * %d = %d\n", x, y, x*y)
+    //Division
+    fmt.Printf(" division : %d / %d = %d\n", x, y, x/y)
+    //Modulus
+    fmt.Printf(" remainder : %d %% %d = %d\n", x, y, x%y)
+	fmt.Println()
+	// Floating-Point Types in Go
 }
