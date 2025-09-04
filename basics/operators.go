0aabc6f32f58df74b2275fb8532d704507fe3527
Pranesh <praneshmadhan646@gmail.com>
2025-09-04 20:13:27 +0530

    added about Arthematic Operators in go with comments for understanding
    Co-authored-by: Pranesh S <praneshpalamedu17@gmail.com>

---
+++ basics/operators.go
@@ -0,0 +1,14 @@
+package main
+import "fmt"
+func main(){
+	// Arithmetic Operators
+	a := 52
+	b := 64
+	fmt.Printf("Addition of %d and %d is %d\n",a,b,a+b)
+	fmt.Printf("Subtraction of %d and %d is %d\n",a,b,a-b)
+	fmt.Printf("Multiplication of %d and %d is %d\n",a,b,a*b)
+	fmt.Printf("Division of %d and %d is %d\n",a,b,a/b)
+	fmt.Printf("Modulus of %d and %d is %d\n",a,b,a%b)
+	fmt.Println()
+	
+}
