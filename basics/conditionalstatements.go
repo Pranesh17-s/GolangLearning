5fd784f74f18e88fb1ce68853d4f00297b03250e
Pranesh <praneshmadhan646@gmail.com>
2025-09-05 15:35:32 +0530

    craeted contidional statements file and added about switch cases condion in go
    Co-authored-by: Pranesh S <praneshpalamedu17@gmail.com>

--- basics/conditionalstatements.go
+++ basics/conditionalstatements.go
@@ -38,4 +38,24 @@ func main() {
 	}else{
 		fmt.Printf("All numbers are equal\n")
 	}
+	//switch statement
+	var day int = 4
+	switch day {
+	case 1:
+		fmt.Println("Monday")
+	case 2:
+		fmt.Println("Tuesday")
+	case 3:
+		fmt.Println("Wednesday")
+	case 4:
+		fmt.Println("Thursday")
+	case 5:
+		fmt.Println("Friday")
+	case 6:
+		fmt.Println("Saturday")
+	case 7:
+		fmt.Println("Sunday")
+	default:
+		fmt.Println("Invalid day")
+	}
 }
