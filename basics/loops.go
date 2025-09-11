e4df02dea15fbe4275f9813925b3dfcca902a637
Pranesh <praneshmadhan646@gmail.com>
2025-09-11 21:31:09 +0530

    added about simple loop infinite loop using for loop go
    Co-authored-by: Pranesh S <praneshpalamedu17@gmail.com>

--- basics/loops.go
+++ basics/loops.go
@@ -2,8 +2,14 @@ package main
 import "fmt"
 func main(){
 	// This is a introduction on loops in go
-	for i:=0;i<5;i++{
-		fmt.Printf("%v\n",i)
+	// Go has only one loop that is for loop you can use that as while loop.
+	// for i:=0;i<5;i++{
+	// 	fmt.Printf("%v\n",i)
+	// }
+	// fmt.Println("Loop ended")
+	
+	//For infinite loop
+	for{
+		fmt.Println("This is infinite loop")
 	}
-	fmt.Println("Loop ended")
 }
